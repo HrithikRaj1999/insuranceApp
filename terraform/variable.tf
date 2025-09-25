540bacb81f92a65e74cb4cc1f8dcaf0f50986573
Hrithik Raj <hrkraj99@gmail.com>
2025-09-26 00:25:14 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,27 +1,28 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy into"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1" # Mumbai
 }
 
 variable "project_name" {
-  description = "Project name"
+  description = "Project name (used for tags and names)"
   type        = string
-  default     = "insurance-claims"
+  default     = "insuranceApp"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.medium"
 }
 
 variable "key_pair_name" {
-  description = "Name of AWS key pair"
+  description = "Existing EC2 key pair name in the chosen region"
   type        = string
 }
 
 variable "github_repo" {
-  description = "GitHub repository URL"
+  description = "GitHub repository to clone on boot"
   type        = string
+  default     = "https://github.com/HrithikRaj1999/insuranceApp"
 }
